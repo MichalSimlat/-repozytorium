2ee198ed0a125f8095a735478603ae856ee3c6a4
MichalSimlat <130637245+MichalSimlat@users.noreply.github.com>
2023-04-13 08:18:56 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/klasy/Osoba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.klasy
+{
+    public class Osoba
+    {
+        protected string Imie { get; set; }
+        protected string Nazwisko { get; set; }
+        protected char Plec { get; set; }
+        protected DateTime Dataurodzenia { get; set; }
+
+        public Osoba(string imie, string nazwisko, char plec, DateTime dataurodzenia)
+        {
+            imie = imie;
+            nazwisko = nazwisko;
+            plec = plec;
+            dataurodzenia = dataurodzenia;
+        }
+        public override string ToString()
+        {
+            return Imie + " " + Nazwisko + "\n "
+                + " Dataurodzenia:" + Dataurodzenia.ToString("D");
+        }
+
+
+
+    }
+}
